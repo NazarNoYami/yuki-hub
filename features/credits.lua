647d7d4160588e640a4bd36eccd6854a4aec1a8c
NazarMaou <113527325+NazarNoYami@users.noreply.github.com>
2026-07-16 13:40:46 +0700

    feat: features/credits.lua

---
+++ features/credits.lua
@@ -0,0 +1,9 @@
+-- Credits Tab
+local YH = _G.YH
+local T = YH.Tabs.Credits
+local S = T:Section({ Title = "Info" })
+S:Button({ Title = "Yuki Hub v5.0", Desc = "Made for Tuan | WindUI | Modular", Callback = function() end })
+S:Space()
+S:Button({ Title = "Features:", Desc = "ESP, Aimbot, Visuals, Misc, HUD", Callback = function() end })
+S:Space()
+S:Button({ Title = "Merged from:", Desc = "Essential Script + Notties Script + Yuki Hub", Callback = function() end })
